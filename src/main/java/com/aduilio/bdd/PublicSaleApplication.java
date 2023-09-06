6aac1358daa8e3d64690fae7b112722180b9b913
Aduilio Silva <developer.aduilio@gmail.com>
2023-09-06 16:38:30 -0300

    Create project

---
+++ src/main/java/com/aduilio/bdd/PublicSaleApplication.java
@@ -0,0 +1,13 @@
+package com.aduilio.bdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublicSaleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aduilio/bdd/PublicSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aduilio.bdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublicSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
